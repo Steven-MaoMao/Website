9af33a71bc99c4f952342a526a189b45477ca31a
hathat <mjdfx120312@163.com>
2022-10-24 16:10:50 +0800

    2022-10-24

---
+++ js/index.js
@@ -0,0 +1,54 @@
+var flag = 0;
+var oldTitle = "";
+
+function changePerSec() {
+    if (flag == 0) {
+        document.title = "★☆★" + oldTitle + "★☆★";
+        $("#search_input").attr("placeholder", "╭(°A°`)╮");
+        flag = 1;
+    } else {
+        document.title = "☆★☆" + oldTitle + "☆★☆";
+        $("#search_input").attr("placeholder", "╭(`°A°)╮");
+        flag = 0;
+    }
+}
+
+$(function () {
+    oldTitle = document.title;
+    setInterval("changePerSec()", 1000);
+    $("#header").animate({ top: "0px", opacity: "1" });
+    $("#main").delay(250).animate({ top: "0px", opacity: "1" });
+    $("#footer").delay(500).animate({ top: "0px", opacity: "1" });
+})
+
+function beianMouseIn(e) {
+    e.style.color = "red";
+}
+
+function beianMouseOut(e) {
+    e.style.color = "black";
+}
+
+function viceTitleOver(e) {
+    e.style.cssText = "border: 1px solid black; box-shadow: 0px 2px 2px -1px black;"
+}
+
+function viceTitleOut(e) {
+    e.style.cssText = "border: none; box-shadow: none;"
+}
+
+function viceTitleDown(e) {
+    e.style.cssText = "box-shadow: 0px 2px 2px -1px black inset;"
+}
+
+function contentOver(e) {
+    e.style.cssText = "border: 1px solid black; box-shadow: 0px 4px 4px -1px black; margin: 9px;"
+}
+
+function contentOut(e) {
+    e.style.cssText = "border: none; box-shadow: none; margin: 10px;"
+}
+
+function contentDown(e) {
+    e.style.cssText = "box-shadow: 0px 4px 4px -1px black inset;"
+}
---
+++ js/messageboard.js
@@ -0,0 +1,139 @@
+var rows;
+
+function getParam(paramName) {
+    if (window.location.href.split("?").length == 1) {
+        return null;
+    }
+    var paramList = window.location.href.split("?")[1].split("&");
+    for (var index in paramList) {
+        if (paramList[index].split("=").length == 1) {
+            continue;
+        }
+        if (paramList[index].split("=")[0] == paramName) {
+            return paramList[index].split("=")[1];
+        }
+    }
+    return null;
+}
+
+var page = getParam("page");
+if (page == null) {
+    page = 1;
+}
+
+$.ajax({
+    url: "http://150.158.48.232:5600/getmessage?page=" + String(page),
+    type: 'get',
+    data: {},
+    success: function (res) {
+        rows = res.pop().num;
+
+        var last_message_id = -1;
+        for (var index in res) {
+            if (res[index].m_id != last_message_id) {
+                var messageContainer = $("<div class='container' id='" + res[index].m_id + "'><div class='message'><div class='personal_info'><div class='name'>" + res[index].m_name + "</div><div class='time_comment_button'><div class='time'>" + res[index].m_time + "</div><div class='comment_button' onmouseover='beianMouseIn(this)' onmouseout='beianMouseOut(this)' onclick='comment(" + res[index].m_id + ")'>评论</div></div></div><div class='text'>" + res[index].m_message + "</div></div><div class='write_comment' style='display: none;'><div style='height: 20px; display: flex; align-items: center; font-size: small;'>昵称：</div><input type='text' class='write_comment_name'><div style='height: 20px; display: flex; align-items: center; font-size: small;'>评论：</div><textarea class='write_comment_text'></textarea><button class='send_comment_button' onclick='sendComment(" + res[index].m_id + ")'>发送</button></div><div class='show_comment'></div></div>");
+                $("#show_message").append(messageContainer);
+                last_message_id = res[index].m_id;
+            }
+        }
+
+        for (var index in res) {
+            if (res[index].c_id != null) {
+                var commentContainer = $("<div class='comment'><div class='comment_info'><div class='comment_name'>" + res[index].c_name + "</div><div class='comment_time'>" + res[index].c_time + "</div></div><div class='comment_text'>" + res[index].c_comment + "</div></div>");
+                $("#" + res[index].m_id + ">.show_comment").append(commentContainer);
+            }
+        }
+
+        if (page != 1) {
+            var lastPage = $("<div id='last_page' onmouseover='pageOver(this)' onmouseout='pageOut(this)' onmousedown='lastPage(this)'>上一页</div>");
+            var startPage = $("<div id='start_page' onmouseover='pageOver(this)' onmouseout='pageOut(this)' onmousedown='startPage(this)'>1</div>");
+            var ellipsis = $("<div class='ellipsis'>...</div>");
+            $("#select_page").append(lastPage);
+            $("#select_page").append(startPage);
+            $("#select_page").append(ellipsis);
+        }
+        var currentPage = $("<div id='current_page'>" + page + "</div>");
+        $("#select_page").append(currentPage);
+        if (page != Math.ceil(rows / 10)) {
+            var ellipsis = $("<div class='ellipsis'>...</div>");
+            var endPage = $("<div id='end_page' onmouseover='pageOver(this)' onmouseout='pageOut(this)' onmousedown='endPage(this)'>" + Math.ceil(rows / 10) + "</div>");
+            var nextPage = $("<div id='next_page' onmouseover='pageOver(this)' onmouseout='pageOut(this)' onmousedown='nextPage(this)'>下一页</div>");
+            $("#select_page").append(ellipsis);
+            $("#select_page").append(endPage);
+            $("#select_page").append(nextPage);
+        }
+    },
+    error: function (res) {
+        console.log("ajax get error");
+    },
+})
+
+function sendMessage() {
+    var name = $("#write_message_name").val();
+    var message = $("#write_message_text").val();
+
+    $.ajax({
+        url: "http://150.158.48.232:5600/sendmessage",
+        type: 'get',
+        data: { name: name, message: message },
+        success: function (res) { },
+        error: function (res) {
+            console.log("ajax get error");
+        },
+    })
+
+    window.location.href = window.location.href.split("?")[0];
+    $("#write_message_name").val("");
+    $("#write_message_text").val("");
+}
+
+function sendComment(e) {
+    var name = $("#" + e + " .write_comment_name").val();
+    var comment = $("#" + e + " .write_comment_text").val();
+
+    $.ajax({
+        url: "http://150.158.48.232:5600/sendcomment",
+        type: 'get',
+        data: { name: name, comment: comment, parent: e },
+        success: function (res) { },
+        error: function (res) {
+            console.log("ajax get error");
+        },
+    })
+
+    window.location.href = window.location.href.split("?")[0];
+    $("#" + e + " .write_comment_name").val("");
+    $("#" + e + " .write_comment_text").val("");
+}
+
+function comment(e) {
+    $("#" + e + " .write_comment").fadeToggle();
+}
+
+function pageOver(e) {
+    e.style.cssText = "border: 1px solid black; box-shadow: 0px 2px 2px -1px black; margin: 0px;"
+}
+
+function pageOut(e) {
+    e.style.cssText = "border: none; box-shadow: none; margin: 1px;"
+}
+
+function lastPage(e) {
+    e.style.cssText = "box-shadow: 0px 2px 2px -1px black inset;"
+    window.location.href = window.location.href.split("?")[0] + "?page=" + String(Number(page) - 1);
+}
+
+function nextPage(e) {
+    e.style.cssText = "box-shadow: 0px 2px 2px -1px black inset;"
+    window.location.href = window.location.href.split("?")[0] + "?page=" + String(Number(page) + 1);
+}
+
+function startPage(e) {
+    e.style.cssText = "box-shadow: 0px 2px 2px -1px black inset;"
+    window.location.href = window.location.href.split("?")[0];
+}
+
+function endPage(e) {
+    e.style.cssText = "box-shadow: 0px 2px 2px -1px black inset;"
+    window.location.href = window.location.href.split("?")[0] + "?page=" + Math.ceil(rows / 10);
+}
